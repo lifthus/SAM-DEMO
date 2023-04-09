90b74e14fa0160d5f6297d0b9ffed4bbd78ba4cc
lifthus <lifthus531@gmail.com>
2023-04-09 16:58:27 +0900

    stageName path set in greeting-service

--- sam-demo/services/greeting-service/main.go
+++ sam-demo/services/greeting-service/main.go
@@ -11,18 +11,33 @@ import (
 )
 
 func main() {
+	// get environment variables
 	demoGreeting, ok1 := os.LookupEnv("DEMO_GREETING")
 	demoUser, ok2 := os.LookupEnv("DEMO_USER")
-	if !ok1 || !ok2 {
+	demoEnv, ok3 := os.LookupEnv("DEMO_ENV")
+	if !ok1 || !ok2 || !ok3 {
 		log.Fatal("DEMO_GREETING and DEMO_USER must be set")
 	}
-	http.DefaultServeMux.HandleFunc("/greeting/who", func(w http.ResponseWriter, r *http.Request) {
+
+	// separate basePath by environment
+	var basePath string
+	if demoEnv == "production" {
+		basePath = "/demo/greeting"
+	} else if demoEnv == "development" {
+		basePath = "/greeting"
+	} else {
+		log.Fatal("wrong environment")
+	}
+
+	http.DefaultServeMux.HandleFunc(basePath+"/who", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/plain")
 		io.WriteString(w, "I am "+demoUser)
 	})
-	http.DefaultServeMux.HandleFunc("/greeting/hi", func(w http.ResponseWriter, r *http.Request) {
+	http.DefaultServeMux.HandleFunc(basePath+"/hi", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/plain")
 		io.WriteString(w, demoGreeting)
 	})
+
+	// run the lambda function with adapter
 	lambda.Start(httpadapter.NewV2(http.DefaultServeMux).ProxyWithContext)
 }
