c64b3bba5a185e070ce73be88bd62c498f40874b
lifthus <lifthus531@gmail.com>
2023-04-09 16:07:42 +0900

    greeting-service finished

--- sam-demo/services/greeting-service/go.mod
+++ sam-demo/services/greeting-service/go.mod
@@ -1,3 +1,8 @@
 module greeting-service
 
 go 1.19
+
+require (
+	github.com/aws/aws-lambda-go v1.19.1 // indirect
+	github.com/awslabs/aws-lambda-go-api-proxy v0.14.0 // indirect
+)
--- sam-demo/services/greeting-service/main.go
+++ sam-demo/services/greeting-service/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/aws/aws-lambda-go/lambda"
+	httpadapter "github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
+)
+
+func main() {
+	demoGreeting, ok1 := os.LookupEnv("DEMO_GREETING")
+	demoUser, ok2 := os.LookupEnv("DEMO_USER")
+	if !ok1 || !ok2 {
+		log.Fatal("DEMO_GREETING and DEMO_USER must be set")
+	}
+	http.DefaultServeMux.HandleFunc("/greeting/who", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain")
+		io.WriteString(w, "I am "+demoUser)
+	})
+	http.DefaultServeMux.HandleFunc("/greeting/hi", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain")
+		io.WriteString(w, demoGreeting)
+	})
+	lambda.Start(httpadapter.NewV2(http.DefaultServeMux).ProxyWithContext)
 }
